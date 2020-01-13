723c6f307834b85dee2d85157686d003e3edd069
trungtin1998 <48832546+trungtin1998@users.noreply.github.com>
2020-01-13 14:31:55 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -14,7 +14,13 @@ go get && go build
 go install
 ## Using
 # Dir mode: Find all files as well as folders of this 
-# gobuster dir -u https://buffered.io -w ~/wordlists/shortlist.txt -v
+# gobuster dir -u YOUR_WEBSITE_TARGET -w PAHT_TO_WORDLIST -v
 # DNS mode: Find all subdomains of website
-# gobuster dns -d google.com -w ~/wordlists/subdomains.txt
+# gobuster dns -d YOUR_WEBSITE_TARGET -w PAHT_TO_WORDLIST
 
+
+################## XSSSNIPPER ##################
+# Find XSS vulnerability automatically by using some XSS plugins
+git clone https://github.com/gbrindisi/xsssniper.git
+## Using
+# python xsssniper.py -u 'YOUR_WEBSITE_TARGET' --crawl --forms
