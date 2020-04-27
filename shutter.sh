dfbbcc287aca38c47736848243f880f2d30327b5
trungtin1998 <48832546+trungtin1998@users.noreply.github.com>
2020-04-27 13:53:57 +0700

    Create shutter.sh
    This bash shell script installs Shutter and enable Edit Button. Restart comupter after run this script

---
+++ shutter.sh
@@ -0,0 +1,14 @@
+# Install Shutter from APT
+sudo apt-get install shutter -y
+
+# Download 
+cd ~/Downloads/
+wget "https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb"
+wget "https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb"
+
+sudo dpkg -i ~/Downloads/libgoocanvas-common_1.0.0-1_all.deb
+sudo dpkg -i ~/Downloads/libgoocanvas3_1.0.0-1_amd64.deb
+sudo apt-get -f install
+
+wget "https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb"
+sudo apt-get -f install
