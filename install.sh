727f908a446abfb4b2b7d0d6df8da728d9a12ea7
trungtin1998 <48832546+trungtin1998@users.noreply.github.com>
2020-12-28 20:48:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ sudo apt-get install net-tools
 # Intall Ibus-Unikey
 sudo add-apt-repository ppa:ubuntu-vn/ppa
 sudo apt-get update
+sudo apt-get install ibus-unikey
 ibus restart
 
 # Install Vim
@@ -43,9 +44,31 @@ sudo apt-get update
 sudo apt-get -y install sublime-text
 
 # Install VMware
-wget -O ~/vmware.bin https://www.vmware.com/go/getWorkstation-linux
-sudo apt -y install build-essential
-sudo bash ~/vmware.bin
+
+# Install Rar and Unrar
+sudo apt-get install rar unrar
+
+# Install Curl
+sudo apt-get install curl
+
+# Install Multi Terminal - Can devide screen into some parts
+sudo apt-get install terminator
+
+# Install John the Ripper - Crack password
+sudo apt-get -y install john
+
+
+# Install JAVA
+sudo apt-get install -y default-jdk
+sudo apt-get install -y default-jre
+## Downloads Burpsuite and executes bash shell file
+
+# Save battery
+# Install tlp and powertop
+sudo apt-get install -y tlp powertop
+sudo systemctl enable tlp
+sudo systemctl start tlp
+sudo systemctl enable tlp-sleep
 
 # Install Wireshark
 sudo apt-get install wireshark
@@ -63,22 +86,6 @@ sudo apt-get install gns3-gui
 sudo dpkg –add-architecture i386
 sudo apt-get update
 sudo apt-get install gns3-iou
-
-# Install Rar and Unrar
-sudo apt-get install rar unrar
-
-# Install Curl
-sudo apt-get install curl
-
-# Install Valgrind - Tool check memory leak
-sudo apt-get install valgrind
-
-# Install Multi Terminal - Can devide screen into some parts
-sudo apt-get install terminator
-
-# Install John the Ripper - Crack password
-sudo apt-get -y install john
-
 # Install Super Productivity - manage projects
 sudo snap install superproductivity
 
@@ -89,14 +96,5 @@ sudo apt-get install wallch
 sudo add-apt-repository ppa:nixnote/nixnote2-daily
 sudo apt-get install nixnote2 -y
 
-# Install JAVA
-sudo apt-get install -y default-jdk
-sudo apt-get install -y default-jre
-## Downloads Burpsuite and executes bash shell file
-
-# Save battery
-# Install tlp and powertop
-sudo apt-get install -y tlp powertop
-sudo systemctl enable tlp
-sudo systemctl start tlp
-sudo systemctl enable tlp-sleep
+# Install Valgrind - Tool check memory leak
+sudo apt-get install valgrind
